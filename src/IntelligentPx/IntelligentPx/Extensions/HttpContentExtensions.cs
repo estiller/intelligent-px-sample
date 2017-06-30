658eb4fa6160d675c4aab5f7b3cf53f0a2105594
Eran Stiller <git@stiller.blog>
2017-06-30 10:20:45 +0300

    Fix wrong HttpClient reference

---
+++ src/IntelligentPx/IntelligentPx/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace IntelligentPx.Extensions
+{
+    public static class HttpContentExtensions
+    {
+        public static async Task<T> ReadAsAsync<T>(this HttpContent httpContent)
+        {
+            var returnedValue = await httpContent.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(returnedValue);
+        }
+    }
+}
--- src/IntelligentPx/IntelligentPx/Services/PhotoService.cs
+++ src/IntelligentPx/IntelligentPx/Services/PhotoService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using IntelligentPx.Extensions;
 using IntelligentPx.Models;
 using PCLAppConfig;
 
